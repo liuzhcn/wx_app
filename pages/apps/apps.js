050e92b86ef58389821da64d8eb920252584a14b
liuzh <liuzh@sibetech.cn>
2017-12-10 01:48:34 +0800

    Initial commit

---
+++ pages/apps/apps.js
@@ -0,0 +1,84 @@
+// pages/apps/apps.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    grids: [
+      {
+        "name":"校园卡",
+        "icon":"/images/card.png"
+      },
+      {
+        "name":"通讯录",
+        "icon": "/images/addressbook.png"
+      },
+      {
+        "name":"看通知",
+        "icon": "/images/notice.png"
+      },
+      {
+        "name":"查图书",
+        "icon": "/images/book.png"
+      }
+
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
