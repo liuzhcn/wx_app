0207e419eef5554a028252f187897c617632b807
xus <754124808@qq.com>
2018-01-17 21:24:45 +0800

    完善地图界面

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,14 +1,27 @@
 // pages/map/map.js
+var sliderWidth = 96;
 Page({
   data: {
+    tabs: ["校门", "教学楼", "宿舍", "食堂","操场","学院","场馆","公交站","学生活动中心","党员活动中心","校长办公室"],
     markers: [{
-      iconPath: "/resources/others.png",
-      id: 0,
-      latitude: 23.099994,
-      longitude: 113.324520,
-      width: 50,
-      height: 50
+        iconPath: "/images/location-640.png",
+        id: 0,
+        title: "华中师范大学1",
+        latitude: 30.5183503570,
+        longitude: 114.3620538712,
+        width: 20,
+        height: 20
     }],
+    label:{
+      content: "华中师范大学",
+      color:"white",
+      fontSize:12,
+      borderRadius:4,
+      bgColor:"#0093dc",
+      padding:"3",
+      display:"ALWAYS",
+      textAlign:"center"
+    },
     polyline: [{
       points: [{
         longitude: 113.3245211,
@@ -23,14 +36,14 @@ Page({
     }],
     controls: [{
       id: 1,
-      iconPath: '/resources/location.png',
+      iconPath: '',
       position: {
         left: 0,
         top: 300 - 50,
         width: 50,
         height: 50
       },
-      clickable: true
+      tmpIndex:0
     }]
   },
   regionchange(e) {
@@ -41,5 +54,64 @@ Page({
   },
   controltap(e) {
     console.log(e.controlId)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  chooseItem: function (e) {
+    
   }
 })
