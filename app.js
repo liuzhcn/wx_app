304be2bdaafc4ac1063049a1f2cedfe6f39fac58
liuzhcn <liuzh.dev@qq.com>
2018-02-25 21:07:33 +0800

    格式化代码，调整version变量的保存位置

--- app.js
+++ app.js
@@ -1,51 +1,53 @@
 //app.js
 App({
-  onLaunch: function () {
- 
-   
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+    onLaunch: function () {
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+
+        // 获取用户信息
+        wx.getSetting({
+            success: res => {
+                if (res.authSetting['scope.userInfo']) {
+                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                    wx.getUserInfo({
+                        success: res => {
+                            // 可以将 res 发送给后台解码出 unionId
+                            this.globalData.userInfo = res.userInfo
+
+                            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                            // 所以此处加入 callback 以防止这种情况
+                            if (this.userInfoReadyCallback) {
+                                this.userInfoReadyCallback(res)
+                            }
+                        }
+                    })
+                }
             }
-          })
+        })
+    },
+    /**华中师范大学数据 
+      globalData: {
+        version: '1.2.1',
+        userInfo: null,
+        appUserInfo: null,
+        token: null,
+        contextPath:'https://wxapp.ccnu.edu.cn/wxapp',
+        mapPoint:{
+            latitude: 30.517330,
+            longitude: 114.360600,
         }
       }
-    })
-  },
-/**华中师范大学数据 
-  globalData: {
-    userInfo: null,
-    appUserInfo: null,
-    token: null,
-    contextPath:'https://wxapp.ccnu.edu.cn/wxapp',
-    mapPoint:{
-        latitude: 30.517330,
-        longitude: 114.360600,
-    }
-  }
-  */
-/** 九江学院数据*/
+      */
+    /** 九江学院数据*/
     globalData: {
-      userInfo: null,
-      appUserInfo: null,
-      token: null,
-      contextPath: 'https://small.sibetech.cn/wxapp',
-      mapPoint: {
-          latitude: 29.710460,
-          longitude: 115.995400
-      }
+        version: '1.2.1',
+        userInfo: null,
+        appUserInfo: null,
+        token: null,
+        contextPath: 'https://small.sibetech.cn/wxapp',
+        mapPoint: {
+            latitude: 29.710460,
+            longitude: 115.995400
+        }
     }
 
 })
